406712777ab0e823430f424235e810bce2fa9c38
PNNutkung <pipatpol.t@ku.th>
2016-09-17 00:19:02 +0700

    Set recycler view adapter to mainactivity

--- app/src/main/java/th/in/pnnutkung/skeassistant/activities/MainActivity.java
+++ app/src/main/java/th/in/pnnutkung/skeassistant/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.databinding.DataBindingUtil;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +14,7 @@ import th.in.pnnutkung.skeassistant.callbacks.MainActivityCallback;
 import th.in.pnnutkung.skeassistant.databinding.ActivityMainBinding;
 import th.in.pnnutkung.skeassistant.managers.misc.Constants;
 import th.in.pnnutkung.skeassistant.models.main_activity.MainActivityGridModel;
+import th.in.pnnutkung.skeassistant.recyclerViews.main_activity.MainActivityGridViewAdapter;
 
 public class MainActivity extends AppCompatActivity implements MainActivityCallback {
 
@@ -24,17 +26,24 @@ public class MainActivity extends AppCompatActivity implements MainActivityCallb
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         initGridViewItem();
+        initRecyclerViewAdapter();
+    }
+
+    private void initRecyclerViewAdapter() {
+        MainActivityGridViewAdapter gridViewAdapter = new MainActivityGridViewAdapter(gridModelList, this);
+        binding.mainActivityRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        binding.mainActivityRecyclerView.setAdapter(gridViewAdapter);
     }
 
     private void initGridViewItem() {
-        gridModelList = new ArrayList<MainActivityGridModel>();
+        gridModelList = new ArrayList<>();
         gridModelList.add(new MainActivityGridModel(ContextCompat.getDrawable(this, R.drawable.ic_announcement),"Annoucement"));
     }
 
     @Override
     public void startActivityByItem(int position) {
         switch (position) {
-            case Constants.NOTIFICATION_BTN:
+            case Constants.ANNOUNCEMENT_BTN:
                 break;
             case Constants.SOCIAL_BTN:
                 break;
--- app/src/main/java/th/in/pnnutkung/skeassistant/managers/misc/Constants.java
+++ app/src/main/java/th/in/pnnutkung/skeassistant/managers/misc/Constants.java
@@ -5,7 +5,7 @@ package th.in.pnnutkung.skeassistant.managers.misc;
  * This class contains constants of application.
  */
 public class Constants {
-    public static final int NOTIFICATION_BTN = 0;
+    public static final int ANNOUNCEMENT_BTN = 0;
     public static final int SOCIAL_BTN = 1;
     public static final int DOCUMENT_BTN = 2;
     public static final int VIDEOS_BTN = 3;
