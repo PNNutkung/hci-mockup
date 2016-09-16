482718ed755e03bdc278f084a48967bc2e96b415
PNNutkung <pipatpol.t@ku.th>
2016-09-17 00:25:24 +0700

    Add Glide to dependency

---
+++ app/src/main/java/th/in/pnnutkung/skeassistant/managers/misc/GlideConfiguration.java
@@ -0,0 +1,26 @@
+package th.in.pnnutkung.skeassistant.managers.misc;
+
+import android.content.Context;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.GlideBuilder;
+import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.module.GlideModule;
+
+/**
+ * Created by nut on 17/9/2559.
+ * This class use for config glide.
+ */
+public class GlideConfiguration implements GlideModule {
+
+    @Override
+    public void applyOptions(Context context, GlideBuilder builder) {
+        // Apply options to the builder here.
+        builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888);
+    }
+
+    @Override
+    public void registerComponents(Context context, Glide glide) {
+        // register ModelLoaders here.
+    }
+}
